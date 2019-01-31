9f2f61f607bda1281280f972cd8a385cfb33489d
Florian Lemaitre <florian.lemaitre@cern.ch>
2019-01-31 16:17:59 +0100

    Fixed frequency autodetection

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
 
-ARCH_FLAGS= -mcpu=native
+ARCH_FLAGS= -march=native
 
 
 $(shell mkdir -p obj)
 
-bandwidth: obj/allocation.o obj/bandwidth.o obj/main.o obj/timer.o
-	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) obj/allocation.o obj/bandwidth.o obj/main.o obj/timer.o -o bandwidth
+bandwidth$(SUFFIX): obj/allocation$(SUFFIX).o obj/bandwidth$(SUFFIX).o obj/main$(SUFFIX).o obj/timer$(SUFFIX).o
+	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) obj/allocation$(SUFFIX).o obj/bandwidth$(SUFFIX).o obj/main$(SUFFIX).o obj/timer$(SUFFIX).o -o bandwidth$(SUFFIX)
 
-obj/allocation.o: src/allocation.cpp include/allocation.h
-	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/allocation.cpp -o obj/allocation.o
-obj/bandwidth.o: src/bandwidth.cpp include/bandwidth.h include/stream.h include/omp-helper.h include/simd.h include/timer.h
-	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/bandwidth.cpp -o obj/bandwidth.o
-obj/main.o: src/main.cpp include/bandwidth.h include/allocation.h include/omp-helper.h include/timer.h
-	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/main.cpp -o obj/main.o
-obj/timer.o: src/timer.cpp include/timer.h
-	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/timer.cpp -o obj/timer.o
+obj/allocation$(SUFFIX).o: src/allocation.cpp include/allocation.h
+	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/allocation.cpp -o obj/allocation$(SUFFIX).o
+obj/bandwidth$(SUFFIX).o: src/bandwidth.cpp include/bandwidth.h include/stream.h include/omp-helper.h include/simd.h include/timer.h
+	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/bandwidth.cpp -o obj/bandwidth$(SUFFIX).o
+obj/main$(SUFFIX).o: src/main.cpp include/bandwidth.h include/allocation.h include/omp-helper.h include/timer.h
+	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/main.cpp -o obj/main$(SUFFIX).o
+obj/timer$(SUFFIX).o: src/timer.cpp include/timer.h
+	$(CXX) -std=c++11 -O3 -fopenmp $(ARCH_FLAGS) -Iinclude -c src/timer.cpp -o obj/timer$(SUFFIX).o
 
 clean:
-	rm -rf obj/allocation.o obj/bandwidth.o obj/main.o obj/timer.o
+	rm -rf obj/allocation$(SUFFIX).o obj/bandwidth$(SUFFIX).o obj/main$(SUFFIX).o obj/timer$(SUFFIX).o
 
 .PHONY: clean
--- src/timer.cpp
+++ src/timer.cpp
@@ -4,101 +4,39 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "timer.h"
 
-// Buffer length should be > size+1
-static inline char* seek_after(FILE* fp, char delim, char* buffer, int size) {
-  char* end = buffer + size;
-  char* first = (char*) memchr(buffer, delim, size);
-  int n = 0, i = 0;
-
-  while (!first) {
-    n = fread(buffer, 1, size, fp);
-    buffer[n] = 0;
-    if (n == 0) return NULL;
-    first = (char*) memchr(buffer, delim, size);
-  }
-
-
-  i = (first - buffer) + 1;
-
-
-  memmove(buffer, buffer + i, size - i);
-  n = fread(end - i, 1, i, fp);
-  end[n-i] = 0;
-
-  return buffer;
+static inline Timer::counter_t read_time(void) noexcept {
+  struct timespec t;
+  clock_gettime(CLOCK_MONOTONIC, &t);
+  return (Timer::counter_t) t.tv_nsec + 1000000000ull * (Timer::counter_t) t.tv_sec;
 }
-
-static inline double get_nominal_frequency() {
-  char buffer[128+1];
-  FILE* fp;
-  const char *pattern = "";
-  char* line;
-  char* end = &buffer[128];
-  int n = 0;
-
-  // safety
-  *end = 0;
-
-  fp = fopen("freq", "r");
-  if (!fp) {
-    pattern = "model name";
-    fp = fopen("/proc/cpuinfo", "r");
-    if (!fp) {
-      fprintf(stderr, "Could not open /proc/cpuinfo\n");
-      return 1e9;
-    }
-  }
-  int pattern_length = strlen(pattern);
-
-  n = fread(buffer, 1, 128, fp);
-  buffer[n] = 0;
-  line = n ? buffer : NULL;
-  while (line && *line) {
-    // If we found the good line
-    if (strncmp(pattern, line, pattern_length) == 0) {
-      line = seek_after(fp, '@', buffer, 128);
-
-      if (!line || !*line) break;
-
-      char* after;
-      double f = strtod(buffer, &after);
-
-      if (after && after != line && (after = (char*) memchr(after, 'H', end - after)) && after[1] == 'z') {
-        switch (after[-1]) {
-          case 'G':
-            f *= 1e3;
-            [[fallthrough]];
-          case 'M':
-            f *= 1e3;
-            [[fallthrough]];
-          case 'k':
-          case 'K':
-            f *= 1e3;
-        }
-        return f;
-      }
-    }
-
-    // Retrieve next line
-    line = seek_after(fp, '\n', buffer, 128);
-  }
-
-  fclose(fp);
-  fprintf(stderr, "Could not find nominal frequency\n");
-  return -1.;
-}
-
-
-
-
 
 
 #ifdef __x86_64__
+#include <unistd.h>
 #include <x86intrin.h>
+
+static inline double get_nominal_frequency() {
+  Timer::counter_t t0, t1, c0, c1;
+  double dt, dc;
+
+  t0 = read_time();
+  c0 = _rdtsc();
+
+  usleep(100000); // wait for 0.1 sec
+
+  t1 = read_time();
+  c1 = _rdtsc();
+
+  dt = (Timer::diff_t) t1 - (Timer::diff_t) t0;
+  dt *= 1e-9;
+  dc = (Timer::diff_t) c1 - (Timer::diff_t) c0;
+
+  return dc / dt;
+}
+
 __attribute((noinline)) Timer::counter_t Timer::read(void) noexcept {
   return _rdtsc();
 }
@@ -108,9 +46,7 @@ bool Timer::low_overhead = true;
 
 #else
 __attribute((noinline)) Timer::counter_t Timer::read(void) noexcept {
-  struct timespec t;
-  clock_gettime(CLOCK_MONOTONIC, &t);
-  return (Timer::counter_t) t.tv_nsec + (Timer::counter_t) 1000000000 * (Timer::counter_t) t.tv_sec;
+  return read_time();
 }
 
 double Timer::frequency = 1e9;
@@ -118,20 +54,18 @@ bool Timer::low_overhead = false;
 
 #endif
 
-namespace {
-  static Timer::diff_t compute_overhead(int tries) noexcept {
-    int i;
-    Timer::counter_t t0, t1;
-    Timer::diff_t d, dmin = -1;
-    for (i = 0; i < tries; ++i) {
-      Timer::reset();
-      t0 = Timer::read();
-      t1 = Timer::read();
-      d = (Timer::diff_t) t1 - (Timer::diff_t) t0;
-      dmin = (d > 0 && (dmin < 0 || d < dmin)) ? d : dmin;
-    }
-    return dmin;
+static inline Timer::diff_t compute_overhead(int tries) noexcept {
+  int i;
+  Timer::counter_t t0, t1;
+  Timer::diff_t d, dmin = -1;
+  for (i = 0; i < tries; ++i) {
+    Timer::reset();
+    t0 = Timer::read();
+    t1 = Timer::read();
+    d = (Timer::diff_t) t1 - (Timer::diff_t) t0;
+    dmin = (d > 0 && (dmin < 0 || d < dmin)) ? d : dmin;
   }
+  return dmin;
 }
 
 Timer::diff_t Timer::overhead = compute_overhead(65536) * 0.8;
